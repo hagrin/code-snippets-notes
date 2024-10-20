4199473389da3075cb20a61258d0a3faa33cb085
hagrin <hagrin@gmail.com>
2024-10-20 07:24:44 +0000

    Create webhook-send.cs

---
+++ telegram/webhook-send.cs
@@ -0,0 +1,34 @@
+// Code snippet to use C#.Net (.Net Core 8.0) to send Telegram webhooks
+
+try
+{
+    string strToken = "MYTOKEN";
+    string strURL = "https://api.telegram.org/bot" + strToken + "/sendMessage";
+    string strChannelID = "MYCHANNELID"; // The channel ID will start with a -100 prefix
+    // How to get your channel ID 
+    // Add @userinfobot to your channel
+    // Forward it a message from your channel
+
+    using (var httpClient = new HttpClient())
+    {
+        var requestUri = new Uri(strURL);
+
+        var jsonContent = new StringContent("{\"chat_id\":\"" + strChannelID + "\", \"text\":\"Test Text\"}", Encoding.UTF8, "application/json");
+
+        var response = await httpClient.PostAsync(requestUri, jsonContent);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadAsStringAsync();
+            // Handle the response if needed
+        }
+    }
+}
+catch (Exception ex)
+{
+    string strError = ex.ToString();
+}
+finally
+{
+    this.Close();
+}

77855468cac8206357b4c93c890281b58ee90c30
hagrin <hagrin@gmail.com>
2024-10-21 01:13:25 +0000

    Create test.cs

---
+++ telegram/test.cs
@@ -0,0 +1,30 @@
+// Code snippet to use C#.Net (.Net Core 8.0) to send Telegram webhooks
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace TestNamespace
+{
+    class TestClass 
+    { 
+        static async Task Main(string[] args)
+        {
+            await RunTest();
+        }
+        static async Task RunTest()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}

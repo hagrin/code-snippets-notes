7e8053a791f24a6931e315108862d67a028fc86f
hagrin <hagrin@gmail.com>
2024-11-12 06:45:24 +0000

    Create emoji-react.js

---
+++ discordjs/emoji-react.js
@@ -0,0 +1,14 @@
+// Code snippet to react to messages with emojis
+
+// To add a reaction you use message.react('unicode') where unicode = in Discord you need to type backslash then the emoji like \:emojiexample: and then use that unicode
+// ex:
+        //message.react('👍');
+        //message.react('👎'); 
+        //message.react('😄');
+        //message.react('692480840948777010');
+        //message.react('692434902892806174');
+
+// You will get an actual emoji if it's a standard emoji, you will get a long ID if it's a custom emoji
+
+message.react('😄');
+message.react('692480840948777010');
